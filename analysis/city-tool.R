c055802554dec460801a8366ea531535bccde457
James Geddes <geddes.james@me.com>
2012-10-31 18:48:28 +0000

    Reworking Manchester analysis.

--- analysis/city-tool.R
+++ analysis/city-tool.R
@@ -4,29 +4,29 @@
 library(plyr)
 library(reshape2)
 
-## Regional emissions data
-load("../data/emissions/data/ukreg.Rdata")
+## Regional energy and emissions data
+load("../data/regional/data/ukreg.Rdata")
 
 ## Cities of interest
-cities <- read.table(header = TRUE, text = "
-city la_name
-Manchester Bolton
-Manchester Bury
-Manchester Oldham
-Manchester Rochdale
-Manchester Stockport
-Manchester Tameside
-Manchester Trafford
-Manchester Wigan
-Manchester Manchester
-Manchester Salford
-Birmingham Birmingham
-Leeds Leeds
-Nottingham Nottingham
-Newcastle 'Newcastle upon Tyne'
+city_codes <- read.table(header = TRUE, text = "
+city geography_code geography_name
+Manchester E08000001 Bolton
+Manchester E08000002 Bury
+Manchester E08000003 Manchester
+Manchester E08000004 Oldham
+Manchester E08000005 Rochdale
+Manchester E08000006 Salford
+Manchester E08000007 Stockport
+Manchester E08000008 Tameside
+Manchester E08000009 Trafford
+Manchester E08000010 Wigan
+Birmingham E08000025 Birmingham
+Leeds E08000035 Leeds
+Nottingham E06000018 Nottingham
+Newcastle E08000021 'Newcastle upon Tyne'
 ")
 
-## High-level sectors
+## High-level emissions sectors
 sector0 <- read.table(header = TRUE, text = "
 sector_code sector0
 A 'Ind. & Comm.'
