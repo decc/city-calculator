bfec864bf4263ac0d021eea16566742713fd31d1
James Geddes <geddes.james@me.com>
2012-11-01 22:50:35 +0000

    Units.

--- data/physics/data/conversion.R
+++ data/physics/data/conversion.R
@@ -1,19 +1,35 @@
 ## Physical constants
-## ------------------
+## ==================
 ##
-## Includes energy conversion factors, calorific values, and emissions factors.
-
-Source: DUKES 2012
-
-energy.units = list(J = 1, Wh = 60 * 60)
-
-## "Small-c" calorie. Source: International Steam Table calorie (1956)
-## see, eg, http://en.wikiepedia.org/wiki/Calorie
+## Energy conversion factors, calorific values, and emissions factors.
 ##
-energy.units = c(energy.units, cal = 4.1868 * energy.units$J, )
 
-## Tonne of oil equivalent. Source: DUKES 2012, Annex A
+## Energy conversion factors
+## -------------------------
 
+energy.units = list(J = 1, Wh = 60 * 60 * 1)
+
+## "Small-c" calorie.
+## Definition: International Steam Table calorie (1956)
+## See, eg, http://en.wikipedia.org/wiki/Calorie
+## This also appears to be the definition used by DUKES
+
+energy.units = c(energy.units, list(cal = 4.1868 * energy.units$J))
+
+## Tonne of oil equivalent.
+## Definition: OECD/IEA definition, also the one used by DUKES
+
+energy.units = c(energy.units, list(toe = 10e7 * energy.units$cal))
+
+## SI prefixes
+
+si.prefix = list(k = 1e3, M = 1e6, G = 1e9, T = 1e12, P = 1e15, E
+  = 1e18, Z = 1e21, Y = 1e24)
+
+## to_base_unit
+
+to_base_unit <- function(q) {}
+  
 
 
 ## change_units(v, from, to)
