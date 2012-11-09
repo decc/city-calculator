49fd5bc75794a4d0b3024bcde04b562097cfb06b
James Geddes <geddes.james@me.com>
2012-11-09 15:49:18 +0000

    Working on untis.

---
+++ R/units.R
@@ -0,0 +1,18 @@
+### Units - A physical quantity package for R
+
+## Notes:
+## - Ratio scales only -- no degrees Celsius to Kelvin
+## - Currently uses SI as the underlying basis
+## - Does not do conversions between coherent systems
+
+## Data structures
+## ---------------
+
+## Should be package local in the end
+
+## dimensions:
+## symbol (in "[]"?)
+## 
+## 
+
+as.quantity <- function(v, units = NULL, dimension = NULL)
